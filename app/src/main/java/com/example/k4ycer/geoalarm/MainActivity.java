5b70e65574d6f0659e6f5f5816c97af2d403c5c1
Eduardo Torres González <k4ycer@hotmail.com>
2018-11-18 12:59:12 -0600

    Add button toolbar

--- app/src/main/java/com/example/k4ycer/geoalarm/MainActivity.java
+++ app/src/main/java/com/example/k4ycer/geoalarm/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.k4ycer.geoalarm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
     }
 }
