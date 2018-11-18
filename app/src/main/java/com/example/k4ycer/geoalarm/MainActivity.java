bde76d6fbd5bfcfde3e2eb074710d01ea8278559
Eduardo Torres González <k4ycer@hotmail.com>
2018-11-18 13:28:14 -0600

    Add alarm button takes you to AddAlarm activity

--- app/src/main/java/com/example/k4ycer/geoalarm/MainActivity.java
+++ app/src/main/java/com/example/k4ycer/geoalarm/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.k4ycer.geoalarm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,5 +19,17 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.btnAddAlarm:
+                Intent i = new Intent(MainActivity.this, AddAlarm.class);
+                startActivity(i);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
